2033ea3ba969605476f674af1a8aa1c4a5688611
katie <113192783+katiekhamarkhanova@users.noreply.github.com>
2023-11-14 09:23:14 -0500

    vitevitevite

--- album-vite/js/index.js
+++ album-vite/js/index.js
@@ -1,9 +1,9 @@
 document.querySelector(".btn").addEventListener("click", function () {
-    if(document.body.classList.contains("light-mode")){
-        document.body.classList.add("dark-mode");
-        document.body.classList.remove("light-mode");
-    } else {
-        document.body.classList.add("light-mode");
-        document.body.classList.remove("dark-mode");
-    }
-})
+  if (document.body.classList.contains("light-mode")) {
+    document.body.classList.add("dark-mode");
+    document.body.classList.remove("light-mode");
+  } else {
+    document.body.classList.add("light-mode");
+    document.body.classList.remove("dark-mode");
+  }
+});
--- album-vite/js/main.js
+++ album-vite/js/main.js
@@ -7,7 +7,8 @@ import viteLogo from '../public/vite.svg';
 /* document.querySelector('#app').innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+      <img src="${viteLogo}" class="logo" alt="V
+      ite logo" />
     </a>
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
