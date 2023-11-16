fdd57f1e836f5ccb1fad7c23a6be84403bb0eb7c
katie <113192783+katiekhamarkhanova@users.noreply.github.com>
2023-11-16 09:26:25 -0500

    vite

--- album-vite/js/index.js
+++ album-vite/js/index.js
@@ -1,4 +1,15 @@
+function switchModes() {
 document.querySelector(".btn").addEventListener("click", function () {
+  if (document.body.classList.contains("light-mode")) {
+    document.body.classList.add("dark-mode");
+    document.body.classList.remove("light-mode");
+  } else {
+    document.body.classList.add("light-mode");
+    document.body.classList.remove("dark-mode");
+  }
+})};
+switchModes();
+document.querySelector(".navbar").addEventListener("click", function (switchModes) {
   if (document.body.classList.contains("light-mode")) {
     document.body.classList.add("dark-mode");
     document.body.classList.remove("light-mode");
