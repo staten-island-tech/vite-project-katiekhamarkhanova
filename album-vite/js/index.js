bd1180a138ac2538987afd8b0f0bdc71e3766105
katie <113192783+katiekhamarkhanova@users.noreply.github.com>
2023-11-21 09:25:46 -0500

    vitevite

--- album-vite/js/index.js
+++ album-vite/js/index.js
@@ -1,21 +1,18 @@
-function switchModes() {
-  document.querySelector(".btn").addEventListener("click", function () {
-    if (document.body.classList.contains("light-mode")) {
-      document.body.classList.add("dark-mode");
-      document.body.classList.remove("light-mode");
-    } else {
-      document.body.classList.add("light-mode");
-      document.body.classList.remove("dark-mode");
-    }
-    document.querySelector(".btn").addEventListener("click", function () {
-      if (document.div.classList.contains("light-navbar")) {
-        document.div.classList.add("dark-navbar");
-        document.div.classList.remove("light-navbar");
-      } else {
-        document.div.classList.add("light-navbar");
-        document.div.classList.remove("dark-navbar");
-      }
-    });
-  });
-}
-switchModes();
+document.querySelector(".btn").addEventListener("click", function () {
+  if (document.body.classList.contains("light-mode")) {
+    document.body.classList.add("dark-mode");
+    document.body.classList.remove("light-mode");
+  } else {
+    document.body.classList.add("light-mode");
+    document.body.classList.remove("dark-mode");
+  }
+});
+document.querySelector(".btn").addEventListener("click", function () {
+  if (document.nav.classList.contains("light-navbar")) {
+    document.nav.classList.add("dark-navbar");
+    document.nav.classList.remove("light-navbar");
+  } else {
+    document.nav.classList.add("light-navbar");
+    document.nav.classList.remove("dark-navbar");
+  }
+});
