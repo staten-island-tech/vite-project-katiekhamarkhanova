dbdac2716b68c24f2cdd4c3977c933f7db99787e
katie <113192783+katiekhamarkhanova@users.noreply.github.com>
2023-11-27 20:11:56 -0500

    vite

--- album-vite/js/dom.js
+++ album-vite/js/dom.js
@@ -6,8 +6,13 @@ const DOMSelectors = {
   h2s: document.querySelectorAll("h2"),
   h3s: document.querySelectorAll("h3"),
   imgHTML: document.querySelectorAll("img"),
+  popBtn: document.querySelector(".pop-button"),
+  rockBtn: document.querySelector(".rock-button"),
+  jazzBtn: document.querySelector(".jazz-button"),
+  kpopBtn: document.querySelector(".kpop-button"),
+  button: document.querySelectorAll("button"),
 };
-
+/* 
 function addCard() {
   albums.forEach((albums) =>
     DOMSelectors.app.insertAdjacentHTML(
@@ -23,4 +28,53 @@ function addCard() {
     )
   );
 }
-addCard();
+addCard(); */
+
+function removeCard() {
+  const buttons = document.querySelectorAll("button");
+  buttons.forEach((button) => {
+    button.addEventListener("click", function (event) {
+      const card = event.currentTarget.parentElement; 
+      card.remove(); 
+    })
+  });
+}
+
+function changeCards(y, x) {
+    albums.map((y) => removeCard(y),
+    x.forEach((x) =>
+        (DOMSelectors.app.insertAdjacentHTML(
+          "beforeend",
+          `<div class="card">
+    <h2>${x.title}</h2>
+    <h3>${x.artist}</h3>
+    <img src="${x.coverImage}" alt=""></img>
+    <div class ="alignButton">
+    <button type="button" id="btn">Add to Cart</button>
+    </div>
+    </div>`
+        ))
+      )
+    )
+        }
+
+
+function filterCardsKpop() {
+  DOMSelectors.kpopBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    const kpopCards = albums.filter((albums) => albums.genre.includes("kpop"));
+    const popCards = albums.filter((albums) => albums.genre.includes("pop"));
+    changeCards(popCards, kpopCards);
+  });
+}
+filterCardsKpop();
+
+function filterCardsPop() {
+  DOMSelectors.popBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    const popCards = albums.filter((albums) => albums.genre.includes("pop"));
+    changeCards(albums, popCards);
+  });
+}
+filterCardsPop();
+
--- album-vite/js/index.js
+++ album-vite/js/index.js
@@ -2,9 +2,17 @@ document.querySelector(".btn").addEventListener("click", function () {
   if (document.body.classList.contains("light-mode")) {
     document.body.classList.add("dark-mode");
     document.body.classList.remove("light-mode");
+    
   } else {
     document.body.classList.add("light-mode");
     document.body.classList.remove("dark-mode");
+  }
+  if (document.nav.classList.contains("light-navbar")) {
+    document.nav.classList.add("dark-navbar");
+    document.nav.classList.remove("light-navbar");
+  } else {
+    document.nav.classList.add("light-navbar");
+    document.nav.classList.remove("dark-navbar");
   }
 });
 document.querySelector(".btn").addEventListener("click", function () {
